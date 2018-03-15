9235d3e938482834ce048782b7503d55292c6636
CokePokes <cokepokes@gmail.com>
2018-03-15 06:17:08 -0600

    initial push

--- fucklargetitles/fucklargetitles.mm
+++ fucklargetitles/fucklargetitles.mm
@@ -15,7 +15,8 @@
 
 #import <Foundation/Foundation.h>
 #import "CaptainHook/CaptainHook.h"
-#include <notify.h> // not required; for examples only
+
+#import <UIKit/UIKit.h>
 
 // Objective-C runtime hooking using CaptainHook:
 //   1. declare class using CHDeclareClass()
@@ -43,51 +44,18 @@
 @end
 
 
-@class ClassToHook;
+CHDeclareClass(UINavigationBar);
 
-CHDeclareClass(ClassToHook); // declare class
-
-CHOptimizedMethod(0, self, void, ClassToHook, messageName) // hook method (with no arguments and no return value)
+CHOptimizedMethod0(self, BOOL, UINavigationBar, prefersLargeTitles)
 {
-	// write code here ...
-	
-	CHSuper(0, ClassToHook, messageName); // call old (original) method
-}
-
-CHOptimizedMethod(2, self, BOOL, ClassToHook, arg1, NSString*, value1, arg2, BOOL, value2) // hook method (with 2 arguments and a return value)
-{
-	// write code here ...
-
-	return CHSuper(2, ClassToHook, arg1, value1, arg2, value2); // call old (original) method and return its return value
-}
-
-static void WillEnterForeground(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
-{
-	// not required; for example only
-}
-
-static void ExternallyPostedNotification(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
-{
-	// not required; for example only
+    return NO;
 }
 
 CHConstructor // code block that runs immediately upon load
 {
 	@autoreleasepool
 	{
-		// listen for local notification (not required; for example only)
-		CFNotificationCenterRef center = CFNotificationCenterGetLocalCenter();
-		CFNotificationCenterAddObserver(center, NULL, WillEnterForeground, CFSTR("UIApplicationWillEnterForegroundNotification"), NULL, CFNotificationSuspensionBehaviorCoalesce);
-		
-		// listen for system-side notification (not required; for example only)
-		// this would be posted using: notify_post("com.cokepokes.fucklargetitles.eventname");
-		CFNotificationCenterRef darwin = CFNotificationCenterGetDarwinNotifyCenter();
-		CFNotificationCenterAddObserver(darwin, NULL, ExternallyPostedNotification, CFSTR("com.cokepokes.fucklargetitles.eventname"), NULL, CFNotificationSuspensionBehaviorCoalesce);
-		
-		// CHLoadClass(ClassToHook); // load class (that is "available now")
-		// CHLoadLateClass(ClassToHook);  // load class (that will be "available later")
-		
-		CHHook(0, ClassToHook, messageName); // register hook
-		CHHook(2, ClassToHook, arg1, arg2); // register hook
+        CHLoadLateClass(UINavigationBar);
+        CHHook0(UINavigationBar, prefersLargeTitles);		
 	}
 }
